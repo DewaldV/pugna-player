81ce645d229de6a43ef3d9359b23c13aa6067572
DewaldV <dewald.viljoen@uswitch.com>
2018-01-08 23:04:47 +0000

    Add code reload for dev env. Add some links on the root url to navigate the api

--- lib/pugna/server.rb
+++ lib/pugna/server.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 module Pugna
   class Server < Sinatra::Base
+    configure :development do
+      register Sinatra::Reloader
+    end
+
     before do
       content_type 'application/json'
+    end
+
+    get '/' do
+      get_links(
+        [
+          { name: 'ping', methods: ['GET'] },
+          { name: 'nextmove', methods: ['POST']}
+        ]
+      ).to_json
     end
 
     get '/ping' do
@@ -15,6 +29,16 @@ module Pugna
     post '/nextmove' do
       JSON.parse request.body.read
       'STAY'.to_json
+    end
+
+    def get_links(links)
+      links.map do |link|
+        {
+          rel: link[:name],
+          url: "#{request.base_url}/#{link[:name]}",
+          methods: link[:methods]
+        }
+      end
     end
   end
 end
